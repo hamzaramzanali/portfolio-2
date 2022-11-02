cf815253649543a200ee6cdbd3e447e9277267db
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-11-01 21:33:03 -0500

    added initial files and structure, added code to use tabs for different pages

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// const styles ={
+//   header: {
+//     background: '#ff0000'
+//   },
+//   h1: {
+//     color: '#ffffff'
+//   }
+// }
+
+function Header() {
+  // TODO: Add a style attribute to header and the h1 element
+
+  return (
+    <header className="header">
+      <h1>Hamza Ramzanali</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/NavTabs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Header from './Header';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <div className="jumbotron-fluid"> 
+    <Header/>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+    </div>
+  );
+}
+
+export default NavTabs;
