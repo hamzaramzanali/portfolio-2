44b8ca1fde4c44ad14024d9c4f2723ee765fd97b
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-11-02 19:09:08 -0500

    added images to aobout page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,6 +1,8 @@
 import { auto } from '@popperjs/core';
 import React from 'react';
 import '../css/AboutMe.css'
+import NWimg from '../../assets/NWimg.png'
+import UICimg from '../../assets/UICimg.svg.png'
 
 const styles = {
   homePgHeader: {
@@ -30,25 +32,29 @@ export default function AboutMe() {
         <h2 style={styles.h2}>About Me</h2>
       </div>
       <div className="container">
-      <div className="card col" styles={styles.card}>
-        <div className="card-body">
-          <h5 className="card-title">Education</h5>
-          <p className="card-text">I am currently a student at the Northwestern Full-Stack Web Development Bootcamp. Previously, I graduated from UIC with a degree in Integrated Health Studies with a concentration in Health Sciences. I graduated in December 2021.</p>
+        <div className="card col" styles={styles.card}>
+          <div className="card-body">
+            <h5 className="card-title">Education</h5>
+            <div className='imgContainer'>
+              <img src={UICimg} alt="educationImg" className='uicIMG'></img>
+              <img src={NWimg} alt="educationImg" className='nwIMG'></img>
+            </div>
+            <p className="card-text">I am currently a student at the Northwestern Full-Stack Web Development Bootcamp. Previously, I graduated from UIC with a degree in Integrated Health Studies with a concentration in Health Sciences. I graduated in December 2021.</p>
+          </div>
         </div>
-      </div>
-      <div className="card col">
-        <div className="card-body">
-          <h5 className="card-title">Career Path</h5>
-          <p className="card-text"> I was working towards becoming a doctor, as that had always been a life-long dream of mine. I realized though that the medicine lifestyle was not for me, so I decided to make a career switch into coding. I feel this is the best profession for me and am really excited to get my career started.</p>
+        <div className="card col">
+          <div className="card-body">
+            <h5 className="card-title">Career Path</h5>
+            <p className="card-text"> I was working towards becoming a doctor, as that had always been a life-long dream of mine. I realized though that the medicine lifestyle was not for me, so I decided to make a career switch into coding. I feel this is the best profession for me and am really excited to get my career started.</p>
+          </div>
         </div>
-      </div>
-      <div className="card col">
-        <div className="card-body">
-          <h5 className="card-title">Right Now</h5>
-          <p className="card-text"> I have completed many projects now that I started my coding career. They can be seen in the Portfolio section of this page. If you have any more questions about me, feel free to contact me through the Contact page. My email and Github are also linked below.
-            Thank you- Hamza!</p>
+        <div className="card col">
+          <div className="card-body">
+            <h5 className="card-title">Right Now</h5>
+            <p className="card-text"> I have completed many projects now that I started my coding career. They can be seen in the Portfolio section of this page. If you have any more questions about me, feel free to contact me through the Contact page. My email and Github are also linked below.
+              Thank you- Hamza!</p>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
