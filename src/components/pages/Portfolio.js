4f854850e36a0445332642fa93d5bf2839b6c643
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-11-02 22:44:03 -0500

    added all my projects to portfolio page with images and styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,69 @@
 import React from 'react';
+import PasswordGenerator from '../../assets/passwordGenerator.png'
+import '../css/Portfolio.css'
+import Portfolio from '../../assets/portfolio1.png'
+import Weather from '../../assets/weatherDashboard.png'
+import ArtApi from '../../assets/project1.png'
+import NoteTaker from '../../assets/noteTaker.png'
+import TeamSpeak from '../../assets/teamSpeak.png'
 
 export default function Blog() {
   return (
     <div>
-      <h1>Portfolio Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className='container portfolioPgContainer'>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body ">
+          <a href='https://hamzaramzanali.github.io/challenge-3-JS/' className='link'>
+            <img src={PasswordGenerator} alt="Password Generator" className='passwordGeneratorImg'></img>
+          </a>
+          <p className='caption'>Password Generator</p>
+        </div>
+      </div>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body">
+          <a href='https://hamzaramzanali.github.io/challenge-2-portfolio/' className='link'>
+            <img src={Portfolio} alt="First Portfolio" className='passwordGeneratorImg'></img>
+          </a>
+          <p className='caption'>First Portfolio</p>
+        </div>
+      </div>
+    </div>
+    <div className='container portfolioPgContainer'>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body">
+          <a href='https://marianellag1.github.io/Explore-Art-Chicago/' className='link'>
+            <img src={ArtApi} alt="Art Institute of Chicago Api" className='weatherImg'></img>
+          </a>
+          <p className='caption'>Art Institute of Chicago Api</p>
+        </div>
+      </div>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body">
+          <a href='https://hamzaramzanali.github.io/challenge-6-weather/' className='link'>
+            <img src={Weather} alt="Weather Dashboard" className='weatherImg'></img>
+          </a>
+          <p className='caption'>Weather Dashboard</p>
+        </div>
+      </div>
+    </div>
+    <div className='container portfolioPgContainer'>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body">
+          <a href='https://aqueous-plateau-30843.herokuapp.com/' className='link'>
+            <img src={NoteTaker} alt="Note Taker" className='weatherImg'></img>
+          </a>
+          <p className='caption'>Note Taker</p>
+        </div>
+      </div>
+      <div className="card col-md-6 portofolio-img-container">
+        <div className="card-body">
+          <a href='https://teamspeak-org.herokuapp.com/' className='link'>
+            <img src={TeamSpeak} alt="Team Speak" className='weatherImg'></img>
+          </a>
+          <p className='caption'>TeamSpeak</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
