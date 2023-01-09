d82c9f9c83820e5ad9385c8c8b79aae45e64359b
hamzaramzanali <hamzaramzanali1@gmail.com>
2023-01-09 13:51:49 -0600

    made changes to portfolio and resume page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,60 +10,90 @@ import TeamSpeak from '../../assets/teamSpeak.png'
 export default function Blog() {
   return (
     <div>
-    <div className='container portfolioPgContainer'>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body ">
-          <a href='https://hamzaramzanali.github.io/challenge-3-JS/' className='link'>
-            <img src={PasswordGenerator} alt="Password Generator" className='passwordGeneratorImg'></img>
-          </a>
-          <p className='caption'>Password Generator</p>
+      <div className='container portfolioPgContainer'>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body ">
+            <a href='https://hamzaramzanali.github.io/challenge-3-JS/' className='link'>
+              <img src={PasswordGenerator} alt="Password Generator" className='passwordGeneratorImg'></img>
+            </a>
+            <p className='caption'>Password Generator</p>
+            <a href="https://github.com/hamzaramzanali/challenge-3-JS">
+              <button className='project-code-btn'>
+                Code
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body">
+            <a href='https://hamzaramzanali.github.io/challenge-2-portfolio/' className='link'>
+              <img src={Portfolio} alt="First Portfolio" className='passwordGeneratorImg'></img>
+            </a>
+            <p className='caption'>First Portfolio</p>
+            <a href="https://github.com/hamzaramzanali/challenge-2-portfolio">
+            <button className='project-code-btn'>
+                Code
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body">
-          <a href='https://hamzaramzanali.github.io/challenge-2-portfolio/' className='link'>
-            <img src={Portfolio} alt="First Portfolio" className='passwordGeneratorImg'></img>
-          </a>
-          <p className='caption'>First Portfolio</p>
+      <div className='container portfolioPgContainer'>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body">
+            <a href='https://marianellag1.github.io/Explore-Art-Chicago/' className='link'>
+              <img src={ArtApi} alt="Art Institute of Chicago Api" className='weatherImg'></img>
+            </a>
+            <p className='caption2'>Art Institute of Chicago Api</p>
+            <a href="https://github.com/Marianellag1/Explore-Art-Chicago">
+            <button className='project-code-btn2'>
+                Code
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body">
+            <a href='https://hamzaramzanali.github.io/challenge-6-weather/' className='link'>
+              <img src={Weather} alt="Weather Dashboard" className='weatherImg'></img>
+            </a>
+            <p className='caption2'>Weather Dashboard</p>
+            <a href="https://github.com/hamzaramzanali/challenge-6-weather">
+            <button className='project-code-btn2'>
+                Code
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-    <div className='container portfolioPgContainer'>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body">
-          <a href='https://marianellag1.github.io/Explore-Art-Chicago/' className='link'>
-            <img src={ArtApi} alt="Art Institute of Chicago Api" className='weatherImg'></img>
-          </a>
-          <p className='caption'>Art Institute of Chicago Api</p>
+      <div className='container portfolioPgContainer'>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body">
+            <a href='https://aqueous-plateau-30843.herokuapp.com/' className='link'>
+              <img src={NoteTaker} alt="Note Taker" className='weatherImg'></img>
+            </a>
+            <p className='caption2'>Note Taker</p>
+            <a href="https://github.com/hamzaramzanali/note-taker-app">
+            <button className='project-code-btn2'>
+                Code
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="card col-md-6 portofolio-img-container">
+          <div className="card-body">
+            <a href='https://teamspeak-org.herokuapp.com/' className='link'>
+              <img src={TeamSpeak} alt="Team Speak" className='weatherImg'></img>
+            </a>
+            <p className='caption2'>TeamSpeak</p>
+            <a href="https://github.com/park-d/team-speak">
+            <button className='project-code-btn2'>
+                Code
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body">
-          <a href='https://hamzaramzanali.github.io/challenge-6-weather/' className='link'>
-            <img src={Weather} alt="Weather Dashboard" className='weatherImg'></img>
-          </a>
-          <p className='caption'>Weather Dashboard</p>
-        </div>
-      </div>
-    </div>
-    <div className='container portfolioPgContainer'>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body">
-          <a href='https://aqueous-plateau-30843.herokuapp.com/' className='link'>
-            <img src={NoteTaker} alt="Note Taker" className='weatherImg'></img>
-          </a>
-          <p className='caption'>Note Taker</p>
-        </div>
-      </div>
-      <div className="card col-md-6 portofolio-img-container">
-        <div className="card-body">
-          <a href='https://teamspeak-org.herokuapp.com/' className='link'>
-            <img src={TeamSpeak} alt="Team Speak" className='weatherImg'></img>
-          </a>
-          <p className='caption'>TeamSpeak</p>
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import resume from '../../assets/New Resume Hamza Ramzanali.docx.pdf'
+import '../css/Resume.css'
+
 
 export default function Resume() {
   return (
     <div>
-      <h1>Resume</h1>
-      <div>
-        <h4>Skills:</h4>
+      <div className="card-resume-pg">
+        <div className="card-body">
+          <h4 className="card-title">Skills:</h4>
+          <p className="card-text">Over my time in the Northwestern Bootcamp, I was able to learn a lot of new skills. Some skills I am proficient in include:
+            Javascript, HTML, CSS, MongoDB, MySQL, Sequelize, GitHub, Heroku, Express, BootStrap, Node.JS, React.JS, among a few others. I am also currently learning how to use AWS, Python, Java and SQL.</p>
+        </div>
       </div>
-      <div>
-        <h4> Downloadable Resume:</h4>
+      <div className="card-resume-pg2">
+        <div className="card-body">
+          <h4 className="card-title">Downloadable Resume:</h4>
         <a target="_blank" rel="noopener noreferrer" href={resume}>
           <button className="resume">
             <b>My Resume</b>
           </button>
         </a>
+        </div>
       </div>
     </div>
   );
